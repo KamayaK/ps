9f5acaf92f95d12d51e0149cdf8652ebbb659a55
Oluwasegun <oladejisteph@gmail.com>
2025-06-30 03:21:52 +0100

    few updates

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -24,11 +24,12 @@ const fetchProducts = async (): Promise<Product[] | null> => {
       console.error('No active session or access token found');
       return null;
     }
-    const response = await fetch('https://pricesniper.fly.dev/products', {
-    // const response = await fetch('http://127.0.0.1:8000/products', {
+    // const response = await fetch('https://pricesniper.fly.dev/products', {
+    const response = await fetch('http://127.0.0.1:8000/products', {
       method: 'GET',
       headers: {
         'Authorization': `Bearer ${session.access_token}`,
+        'X-Refresh-Token': session.refresh_token, 
         'Content-Type': 'application/json',
       },
     });
